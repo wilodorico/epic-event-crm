95e9055639a2531e9b3700edbcd2e192df1edf6f
wilfried <wilfried.odorico@gmail.com>
2025-09-06 09:23:01 +0200

    Add database module with engine, session, and initialization function

---
+++ src/collaborators/infrastructure/database/__init__.py
@@ -0,0 +1,3 @@
+from .db import Base, engine, init_db
+
+__all__ = ["engine", "Base", "init_db"]
---
+++ src/collaborators/infrastructure/database/db.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = "sqlite:///epic.db"
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+# def get_session():
+#     session = SessionLocal()
+#     try:
+#         yield session
+#     finally:
+#         session.close()
+
+
+def init_db():
+    print("Creating tables...")
+    Base.metadata.create_all(bind=engine)
+    print("Tables created successfully!")
