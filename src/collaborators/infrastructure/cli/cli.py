1d81e282593589c5ae871474fa835e75af5f8325
wilfried <wilfried.odorico@gmail.com>
2025-09-25 16:45:59 +0200

    Add contract command to CLI

--- src/collaborators/infrastructure/cli/cli.py
+++ src/collaborators/infrastructure/cli/cli.py
@@ -1,6 +1,7 @@
 import click
 
 from collaborators.infrastructure.cli.commands.collaborator import collaborator
+from collaborators.infrastructure.cli.commands.contract import contract
 from collaborators.infrastructure.cli.commands.customer import customer
 from collaborators.infrastructure.cli.commands.init_db import init_db_command
 
@@ -14,3 +15,4 @@ def cli():
 cli.add_command(init_db_command)
 cli.add_command(collaborator)
 cli.add_command(customer)
+cli.add_command(contract)
