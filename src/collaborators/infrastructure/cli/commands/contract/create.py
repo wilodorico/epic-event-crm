d6a10d9bae7aaa6775f6625788b9a2fc1db165db
wilfried <wilfried.odorico@gmail.com>
2025-09-26 10:20:50 +0200

    Refactor create contract command to use input validation for total and remaining amounts in options

--- src/collaborators/infrastructure/cli/commands/contract/create.py
+++ src/collaborators/infrastructure/cli/commands/contract/create.py
@@ -3,15 +3,23 @@ import click
 from collaborators.application.contract.create_contract_use_case import CreateContractUseCase
 from collaborators.application.services.auth_context import AuthContext
 from collaborators.domain.collaborator.collaborator import Collaborator, Role
+from collaborators.infrastructure.cli.inputs_validator import validate_positive_decimal
 from collaborators.infrastructure.repositories.sqlalchemy_contract_repository import SqlalchemyContractRepository
 from collaborators.infrastructure.repositories.sqlalchemy_customer_repository import SqlalchemyCustomerRepository
 from commons.uuid_generator import UuidGenerator
 
 
 @click.command("create-contract", help="Create a new contract")
-@click.option("--customer-id", prompt="Customer ID", type=str, help="ID of the customer")
-@click.option("--total-amount", prompt="Total Amount", type=float, help="Total amount of the contract")
-@click.option("--remaining-amount", prompt="Remaining Amount", type=float, help="Remaining amount of the contract")
+@click.option("--customer-id", prompt="Customer ID", help="ID of the customer")
+@click.option(
+    "--total-amount", prompt="Total Amount", callback=validate_positive_decimal, help="Total amount of the contract"
+)
+@click.option(
+    "--remaining-amount",
+    prompt="Remaining Amount",
+    callback=validate_positive_decimal,
+    help="Remaining amount of the contract",
+)
 @click.pass_context
 def create_contract(ctx, customer_id, total_amount, remaining_amount):
     session = ctx.obj.get("session") if ctx.obj and "session" in ctx.obj else None
