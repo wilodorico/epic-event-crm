fba0ccc22eb129766d318c0992013f1d251c63ae
wilfried <wilfried.odorico@gmail.com>
2025-10-06 20:29:29 +0200

    Fix authorization error message to use permission name instead of value

--- src/collaborators/application/exceptions/authorization_error.py
+++ src/collaborators/application/exceptions/authorization_error.py
@@ -2,5 +2,5 @@ class AuthorizationError(PermissionError):
     """Exception raised for authorization errors."""
 
     def __init__(self, user, permission):
-        message = f"User '{user.email}' with role '{user.role.value}' does not have permission '{permission.value}'"
+        message = f"User '{user.email}' with role '{user.role.value}' does not have permission '{permission.name}'"
         super().__init__(message)
